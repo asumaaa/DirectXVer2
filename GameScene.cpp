d0f7f09d761cdd2b385926a9ec87ea5d8fb0bd66
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2023-02-17 13:58:35 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.cpp
@@ -0,0 +1,68 @@
+#include "GameScene.h"
+#include "FbxLoader.h"
+#include<fstream>
+#include "sstream"
+#include "stdio.h"
+#include "string.h"
+#include "vector"
+
+#define PI 3.1415
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
+{
+	this->dxCommon_ = dxCommon;
+	this->input_ = input;
+
+	//カメラ初期化
+	Camera::SetInput(input_);
+	Camera::SetDXInput(dxInput);
+	Camera* newCamera = new Camera();
+	newCamera->Initialize();
+	camera_.reset(newCamera);
+	camera_->SetTarget({ 0,0,0 });
+	camera_->SetEye({ 0, 10,-10 });
+
+	//FBX読み込み
+	FbxLoader::GetInstance()->Initialize(dxCommon_->GetDevice());
+	//モデル名を指定してファイル読み込み
+	model0 = FbxLoader::GetInstance()->LoadModelFromFile("cube", "Resources/white1x1.png");
+
+	//デバイスをセット
+	FbxObject3D::SetDevice(dxCommon_->GetDevice());
+	FbxObject3D::SetCamera(camera_.get());
+	FbxObject3D::CreateGraphicsPipeline();
+
+	//オブジェクト初期化
+	object0 = new FbxObject3D;
+	object0->Initialize();
+	object0->SetModel(model0);
+}
+
+void GameScene::Update()
+{
+	//カメラ更新
+	camera_->DebugUpdate();
+	camera_->Update();
+	//コントローラー更新
+	dxInput->InputProcess();
+
+	//オブジェクト更新
+	object0->SetPosition(position);
+	object0->SetScale(scale);
+	object0->SetRotation(rotation);
+	object0->Update();
+
+}
+
+void GameScene::Draw()
+{
+	object0->Draw(dxCommon_->GetCommandList());
+}
---
+++ GameScene.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "memory"
+#include "list"
+
+#include "Input.h"
+#include "DXInput.h"
+#include "DirectXCommon.h"
+#include "Camera.h"
+#include "FbxObject3d.h"
+#include "FbxModel.h"
+
+class GameScene
+{
+	//メンバ関数
+public:
+	GameScene();
+	~GameScene();
+	void Initialize(DirectXCommon* dxCommon, Input* input);
+	//更新
+	void Update();
+	//描画
+	void Draw();
+
+	//メンバ変数
+private:
+	//デバイスとinput
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	DXInput* dxInput = new DXInput();
+	//カメラ
+	std::unique_ptr<Camera> camera_;
+
+	//fbx
+	FbxModel* model0 = nullptr;
+	FbxObject3D* object0 = nullptr;
+
+	//変形行列
+	DirectX::XMFLOAT3 position = { 0.0f,0.0f,0.0f };
+	DirectX::XMFLOAT3 rotation = { 0.0f,0.0f,0.0f };
+	DirectX::XMFLOAT3 scale = { 0.10f,0.001f,0.10f };
+};
